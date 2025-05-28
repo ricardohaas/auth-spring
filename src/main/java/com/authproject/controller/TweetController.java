64f64b19d20ceb739cb49a2eeecd2f5fe50d6047
Luis Ricardo Haas <lhaas01@kenvue.com>
2025-05-28 11:25:24 -0300

    Deleção de tweets, padronização para usar optinal

--- src/main/java/com/authproject/controller/TweetController.java
+++ src/main/java/com/authproject/controller/TweetController.java
@@ -2,16 +2,22 @@ package com.authproject.controller;
 
 import com.authproject.controller.dto.CreateTweetDto;
 import com.authproject.entities.Tweet;
+import com.authproject.entities.User;
 import com.authproject.repository.TweetRepository;
 import com.authproject.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -28,11 +34,34 @@ public class TweetController {
     @PostMapping("/tweets")
     public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDto createTweetDto,
                                             UsernamePasswordAuthenticationToken token){
-        var user  =  userRepository.findById(UUID.fromString(token.getName()));
+        Optional<User> user  =  userRepository.findById(UUID.fromString(token.getName()));
+        if( user.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        }
         var tweet = new Tweet();
         tweet.setContent(createTweetDto.content());
         tweet.setUser(user.get());
         tweetRepository.save(tweet);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
+                                            UsernamePasswordAuthenticationToken token){
+
+        Optional<Tweet> tweet = tweetRepository.findById(tweetId);
+
+        if( tweet.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        }
+
+        if(tweet.get().getUser().getUserId().equals(UUID.fromString(token.getName()))){
+            tweetRepository.deleteById(tweetId);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/authproject/repository/TweetRepository.java
+++ src/main/java/com/authproject/repository/TweetRepository.java
@@ -3,8 +3,6 @@ package com.authproject.repository;
 import com.authproject.entities.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface TweetRepository extends JpaRepository<Tweet, UUID> {
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
 
 }
